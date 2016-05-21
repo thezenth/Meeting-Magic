f50bc6075f9e691617b28be96a0010f87ab4c5d0
Noah Wolfe <Noah Wolfe>
2016-05-21 14:20:22 -0400

    Basic implementation of Factual python API

--- Food/fetch_restaurants.py
+++ Food/fetch_restaurants.py
@@ -1,1 +1,27 @@
 import json
+from factual import Factual
+from factual.utils import circle
+
+lat = 34.058583
+lang = -118.416582
+rad = 1000
+
+key = "LkwWeSeAZT1xCBdQRcAqsjsoKAHQVOm7tE4KzfjX"
+secret_key = "xI8R8pWMoRJspCY40g6rrAsr9s7idQ5JXoGzxEcO"
+factual = Factual(key, secret_key)
+places = factual.table('places')
+data = places.search('coffee').geo(circle(34.058583, -118.416582, 1000)).data()
+json.loads(data)
+
+
+
+"""
+keyInclude = "&KEY=" + key
+url = 'http://api.v3.factual.com/t/places-us?filters={"$and":[{"name":{"$search":"starbucks"}},{"postcode":{"$eq":"90067"}}]}&select=name,address,latitude,longitude' + keyInclude #Get lat and long of all starbucks in 90067 postal code
+r = requests.get(url)
+if r.status_code != 200:
+    print ("Couldn't access API! Error code: " + str(r.status_code))
+    exit()
+print r.text
+json.loads(r.text)
+"""
