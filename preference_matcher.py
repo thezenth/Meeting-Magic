8faed0f25043ed6191dcd73faf66e8afd5c9d822
Noah Wolfe <Noah Wolfe>
2016-05-21 18:50:37 -0400

    Quick update to preference_matcher.py

--- preference_matcher.py
+++ preference_matcher.py
@@ -4,3 +4,9 @@ from user_classes import *
 localUser = yaml.load("User/user_config.yaml")
 otherUser = User("Noah")
 otherUser.add_food_prefs("Gujarati", "Sushi", "Pizza", "Jewish")
+
+luPrefs = localUser.food_prefs
+ouPrefs = otherUser.food_prefs
+
+#def compare(user1, user2):
+#    for a in user1.food_prefs
