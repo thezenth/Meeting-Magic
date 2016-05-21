24f32dca4d1fed8f92a5719f7a440474326527c4
Noah Wolfe <Noah Wolfe>
2016-05-21 13:47:25 -0400

    Created fetch_restaurants.py

--- main.py
+++ main.py
@@ -8,6 +8,15 @@ data = newUser
 stream = file('User/user_config.yaml', 'w')
 yaml.dump(data, stream)
 
+
+stream2 = file('food_categories.yaml', 'r')
+categories = yaml.load(stream2)
+print categories["foods"]["pizza"]
+print categories["dietary_restrictions"][0]
+
+#stream2 = file('food_categories.yaml', #'w')
+#yaml.dump(testDict, stream2#)
+
 # DEBUG
 '''
 print (
