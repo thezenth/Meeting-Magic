274b77352d20b61f0e5029b7f917766511563e81
Noah Wolfe <Noah Wolfe>
2016-05-22 12:31:44 -0400

    Basic food preference matching between two Users

--- preference_matcher.py
+++ preference_matcher.py
@@ -1,12 +1,30 @@
 import yaml
 from user_classes import *
 
-localUser = yaml.load("User/user_config.yaml")
+def compare(user1, user2):
+    same = []
+    if len(user1.food_prefs) >= len(user2.food_prefs):
+        print (len(user1.food_prefs), len(user2.food_prefs))
+        for a in range(len(user2.food_prefs)):
+            print (user2.food_prefs[a], user1.food_prefs)
+            if user2.food_prefs[a] in user1.food_prefs:
+                same.append(user2.food_prefs[a])
+    else:
+        print (len(user1.food_prefs), len(user2.food_prefs))
+        for a in range(len(user1.food_prefs)):
+            print (user1.food_prefs[a], user2.food_prefs)
+            if user1.food_prefs[a] in user2.food_prefs:
+                same.append(user1.food_prefs[a])
+
+    return (same)
+
+f = open("User/user_config.yaml", "r")
+text = f.read()
+yamld = yaml.load(text)
+localUser = yamld #remember, this is the funky human-readable but class instantiating thing, like User(username=bsasf, etc.)
 otherUser = User("Noah")
-otherUser.add_food_prefs("Gujarati", "Sushi", "Pizza", "Jewish")
+otherUser.add_food_prefs("Gujarati", "Mango", "Pizza", "Jewish")
 
-luPrefs = localUser.food_prefs
-ouPrefs = otherUser.food_prefs
-
-#def compare(user1, user2):
-#    for a in user1.food_prefs
+print (compare(
+    localUser, otherUser
+))
