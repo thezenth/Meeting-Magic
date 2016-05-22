f1d8e2ff71ff497f994fd0b9f153fe2cb190cf76
Noah Wolfe <Noah Wolfe>
2016-05-22 09:51:24 -0400

    Basic read of fetched Factual API based on configs

--- Food/fetch_restaurants.py
+++ Food/fetch_restaurants.py
@@ -25,17 +25,28 @@ factual = Factual(key, secret_key)
 places = factual.table('places')
 data = places.search('coffee').geo(circle(34.058583, -118.416582, 1000)).data() #outputs a list of different objects, each object having info on the place
 #pp.pprint(data) #good for debug, takes a little bit
-
+'''
 strm = file("fetch_config.yaml", "w")
-
+stuff = {'get': {"distance":"$distance","address":"address", "hours":["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"], "telephone":"tel"}}
+yamlStuff = yaml.dump(stuff, default_flow_style=False)
+strm.write(yamlStuff)
+strm.close()
+'''
 stream = file("fetch_config.yaml", "r")
 getList = yaml.load(stream)['get']
-for a in range(len(getList)):
-    if a is 'hours':
-        for b in range(len(getList['hours'])):
-            print data[0][a][b]
-    else:
-        print data[0][a]
+
+for a in getList:
+    #just in case we REALLY need to parse a list
+    #if a == 'hours':
+    #    for b in range(len(getList[a])):
+    #        print getList[a][b]
+    #        print data[0][
+    #            getList[a][b]
+    #        ]
+    #else:
+    print data[0][
+        getList[a]
+    ]
 
 #spprint(data[0]["tel"], doPrint=True)
 
