e01f68e6f6bcec210bbce987b54fb0b06991f020
Noah Wolfe <Noah Wolfe>
2016-05-30 14:30:58 -0400

    Return and eventually search for name and address of restaurant

--- food/fetch_restaurants.py
+++ food/fetch_restaurants.py
@@ -104,8 +104,9 @@ def get_restaurants (lati, longi, rad, cat):
                     hours = curr_rest['opening_hours']['weekday_text']
 
         newRest = Restaurant(ident, name, address, rest_latitude, rest_longitude, hours, rating)
-        if newRest.name not in restList:
-            restList.append(newRest.name) # Bit of a hack for now, will have to fix it somehow
+        nameAndAddress = newRest.name + " " + newRest.address
+        if nameAndAddress not in restList:
+            restList.append(nameAndAddress) # Bit of a hack for now, will have to fix it somehow
         pn.tell_node("  Finished parsing restaurant " + str(i))
         pn.tell_node("  " + newRest.__repr__())
         #print (newRest.__repr__)
