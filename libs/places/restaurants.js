fe816ad423392c101536cffa3cdd2c6b9e8f4f6d
Noah Wolfe <Noah Wolfe>
2016-06-01 22:48:47 -0400

    Begin fetch_parse function and added YUIDoc comment

--- libs/places/restaurants.js
+++ libs/places/restaurants.js
@@ -6,13 +6,22 @@ var get_place = fetch.get_place;
 var places = require("./places.js");
 var food_q = places.Food;
 
-var rest_q = food_q;
-rest_q.position = {
-  lat: 34.058583,
-  long: -118.416582
-};
-rest_q.rad = 5000;
-rest_q.cat = "coffee";
-rest_q.rankBy = "prominence";
+/**
+* Designed to fetch data on a restaurant from the Google Places API in JSON format, using the get_place function from fetch.js and Food query template.
+* @method fetch_parse
+* @param {Object} pos A position object.
+* @param {Number} rad The radius of the defined search area circle.
+* @param {String} cat The category of restaurant you are looking for; ex. coffee, pizza, donuts, indian, etc.
+* @param {String} rankBy For the Google Places API, either "distance" (which then ignores radius, and ranks by distance from provided position), or "prominence" (which takes into account rating, mentions on google, etc.)
+*/
+function fetch_parse(pos, rad, cat, rankBy) {
+  var rest_q = food_q;
+  rest_q.position = pos;
+  rest_q.rad = rad;
+  rest_q.cat = cat;
+  rest_q.rankBy = rankBy;
 
+  rest = get_place(rest_q);
+
+}
 //get_place(rest_q);
