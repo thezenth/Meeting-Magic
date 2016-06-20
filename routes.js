3e532a8571873fbbf1f4a2f6dc212c859b6ed97c
Noah Wolfe <Noah Wolfe>
2016-06-20 16:58:52 -0400

    Creation of query string for users included in the meeting

--- routes.js
+++ routes.js
@@ -1,3 +1,5 @@
+var qs = require('qs');
+
 //preferences.js
 prefs = require('./config/preferences');
 Foods = prefs.Foods;
@@ -206,7 +208,7 @@ module.exports = function (app, passport) {
 										var parsedJson = JSON.parse(jsonData);
 										var found_places = parsedJson["found_places"];
 										if (found_places.length > 0) {
-											res.redirect('/results');
+											res.redirect('/results?users=' + qs.stringify( { "users": [req.user.local.email,req.body.otheremail] } ));
 											clearInterval(interval);
 										}
 									}
@@ -254,8 +256,14 @@ module.exports = function (app, passport) {
 
 	app.post('/results', isLoggedIn, function(req, res) {
 		newMeeting = new Meeting({
-
+			users: qs.parse(req.query.users)["users"],
+			location_ref: "",
+			date: "",
+			time: ""
 		});
+		dlog("created a new meeting:" + newMeeting, def_opts);
+		newMeeting.save();
+		res.redirect('/create');
 	});
 
 };
